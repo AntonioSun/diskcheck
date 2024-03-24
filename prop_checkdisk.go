b0a24d3aa8eb951cc9c3744a3687d0267c5cd872
AntonioSun <asun@ats.l5.ca>
2024-03-24 16:57:13 -0400

    - [+] measureReadSpeed() func (& -w) implemented

--- prop_checkdisk.go
+++ prop_checkdisk.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"syscall"
@@ -37,7 +38,7 @@ func checkDisk(diskPath string) {
 	targetSize := availableSpace - GB*uint64(Opts.Spare)
 	chunkSize := targetSize / blockSize * blockSize / uint64(Opts.DataPoints) // dataPoint Interval, Size of each data point
 	debug(fmt.Sprintf("Target file size: %d bytes\n", targetSize), 1)
-	fmt.Printf(" using %dMB with %d speed measurement data points...\n", targetSize/MB, Opts.DataPoints)
+	fmt.Printf(" using %dMB with %d speed-measurement data points...\n", targetSize/MB, Opts.DataPoints)
 
 	// prepare space/data once, before measuring
 	speedData := make([]int, Opts.DataPoints)
@@ -53,9 +54,14 @@ func checkDisk(diskPath string) {
 	measureWriteSpeed(speedData, fileName, buffer, blockSize, chunkSize)
 	printSpeeds("Write speeds", speedData)
 
+	wait := time.Duration(Opts.Wait) * time.Second
+	fmt.Printf("\nWait after write before read for %v for sytem to take a breath...\n", wait)
+	time.Sleep(wait)
+
 	// Read pass
-	// measureReadSpeed(speedData, fileName, buffer, blockSize, chunkSize)
-	// printSpeeds("Read speeds", speedData)
+	fmt.Printf("\nRead back & verify...\n")
+	measureReadSpeed(speedData, fileName, buffer, blockSize, chunkSize)
+	printSpeeds("Read speeds", speedData)
 
 }
 
@@ -80,6 +86,46 @@ func measureWriteSpeed(speedData []int, fileName string, buffer []byte, blockSiz
 			}
 		}
 		file.Sync() // Flush to disk
+		elapsed := time.Since(startTime)
+
+		if Opts.KbSpeed {
+			speedData[p] = (int(chunkSize)*10/KB*1000/int(elapsed.Milliseconds()) + 5) / 10
+		} else {
+			speedData[p] = (int(chunkSize)*10/MB*1000/int(elapsed.Milliseconds()) + 5) / 10
+		}
+		print(".")
+	}
+	print("\n")
+
+	return true
+}
+
+func measureReadSpeed(speedData []int, fileName string, buffer []byte, blockSize, chunkSize uint64) bool {
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return false
+	}
+	defer file.Close()
+
+	// data points loop for each speed measurement
+	data := make([]byte, blockSize)
+	for p := 0; p < Opts.DataPoints; p++ {
+		// chunkSize loop to fill up each data point
+		startTime := time.Now()
+		for i := 0; i < int(chunkSize/blockSize); i++ {
+			n, err := file.Read(data)
+			if err != nil {
+				fmt.Println("Error writing data:", err)
+				return false
+			}
+
+			if n != int(blockSize) || !bytes.Equal(data, buffer) {
+				fmt.Printf("\nData mismatch at data point %d offset %d\n", p, i)
+				return false
+			}
+		}
 		elapsed := time.Since(startTime)
 
 		if Opts.KbSpeed {
