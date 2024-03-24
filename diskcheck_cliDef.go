2cacbe72be8c7124b79e22225869517b1f2a8e20
AntonioSun <asun@ats.l5.ca>
2024-03-24 14:48:29 -0400

    - [+] measureWriteSpeed() implemented

--- diskcheck_cliDef.go
+++ diskcheck_cliDef.go
@@ -22,7 +22,7 @@ type Options struct {
 	DataPoints int  // number of data points for speed measurement
 	KbSpeed    bool // use KB/s to measure speed
 	Debug      int  // debugging level
-	Help       bool // show usage help
+	Help       bool // show this usage help
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -46,7 +46,7 @@ func initVars() {
 	flag.IntVar(&Opts.Debug, "d", 0,
 		"debugging level")
 	flag.BoolVar(&Opts.Help, "h", false,
-		"show usage help")
+		"show this usage help")
 }
 
 func initVals() {
@@ -79,7 +79,7 @@ func initVals() {
 
 }
 
-const usageSummary = "  -sp\tspare the last amount of GB from filling up (DISKCHECK_SP)\n  -p\tnumber of data points for speed measurement (DISKCHECK_P)\n  -k\tuse KB/s to measure speed (DISKCHECK_K)\n  -d\tdebugging level (DISKCHECK_D)\n  -h\tshow usage help (DISKCHECK_H)\n\nDetails:\n\n"
+const usageSummary = "  -sp\tspare the last amount of GB from filling up (DISKCHECK_SP)\n  -p\tnumber of data points for speed measurement (DISKCHECK_P)\n  -k\tuse KB/s to measure speed (DISKCHECK_K)\n  -d\tdebugging level (DISKCHECK_D)\n  -h\tshow this usage help (DISKCHECK_H)\n\nDetails:\n\n"
 
 // Usage function shows help on commandline usage
 func Usage() {
@@ -96,6 +96,15 @@ free for normal operation.
 
 The '-sp','-p' flags can be overridden by environment variables
 'DISKCHECK_SP','DISKCHECK_P', etc
+
+Usage Examples (say disk avail is 267GB):
+  diskcheck -sp 203 /tmp # 64GB write
+  diskcheck -sp 139 -p 200 /tmp # 128GB write
+  ... # 25GB write with -p 400
+
+  or,
+
+  DISKCHECK_SP=139 DISKCHECK_P=200 diskcheck /tmp
 `)
 	os.Exit(0)
 }
--- prop_checkdisk.go
+++ prop_checkdisk.go
@@ -1,9 +1,105 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
+	"syscall"
+	"time"
 )
 
-func checkDisk(path string) {
-	log.Printf("Checking disk under %s\n", path)
+////////////////////////////////////////////////////////////////////////////
+// Constant and data type/structure definitions
+
+const (
+	KB = 1024
+	MB = 1024 * KB
+	GB = 1024 * MB
+)
+
+////////////////////////////////////////////////////////////////////////////
+// Function definitions
+
+func checkDisk(diskPath string) {
+	fmt.Printf("Checking disk under %s", diskPath)
+
+	var stat syscall.Statfs_t
+	info, err := os.Stat(diskPath)
+	checkError(err)
+
+	if !info.IsDir() {
+		checkError(fmt.Errorf("Input '%s' is not a directory.\n", diskPath))
+	}
+
+	// Determine available disk space
+	syscall.Statfs(diskPath, &stat)
+	blockSize := uint64(stat.Bsize) // Size of each data block
+	availableSpace := stat.Bavail * blockSize
+	targetSize := availableSpace - GB*uint64(Opts.Spare)
+	chunkSize := targetSize / blockSize * blockSize / uint64(Opts.DataPoints) // dataPoint Interval, Size of each data point
+	debug(fmt.Sprintf("Target file size: %d bytes\n", targetSize), 1)
+	fmt.Printf(" using %dMB with %d speed measurement data points...\n", targetSize/MB, Opts.DataPoints)
+
+	// prepare space/data once, before measuring
+	speedData := make([]int, Opts.DataPoints)
+	buffer := make([]byte, stat.Bsize)
+	for i := range buffer {
+		buffer[i] = (byte)(i % 512)
+	}
+
+	fileName := diskPath + "/test.dat"
+	defer os.Remove(fileName)
+
+	// Write pass
+	measureWriteSpeed(speedData, fileName, buffer, blockSize, chunkSize)
+	printSpeeds("Write speeds", speedData)
+
+	// Read pass
+	// measureReadSpeed(speedData, fileName, buffer, blockSize, chunkSize)
+	// printSpeeds("Read speeds", speedData)
+
+}
+
+func measureWriteSpeed(speedData []int, fileName string, buffer []byte, blockSize, chunkSize uint64) bool {
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return false
+	}
+	defer file.Close()
+
+	// data points loop for each speed measurement
+	for p := 0; p < Opts.DataPoints; p++ {
+		// chunkSize loop to fill up each data point
+		startTime := time.Now()
+		for i := 0; i < int(chunkSize/blockSize); i++ {
+			_, err := file.Write(buffer)
+			if err != nil {
+				fmt.Println("Error writing data:", err)
+				return false
+			}
+		}
+		file.Sync() // Flush to disk
+		elapsed := time.Since(startTime)
+
+		if Opts.KbSpeed {
+			speedData[p] = (int(chunkSize)*10/KB*1000/int(elapsed.Milliseconds()) + 5) / 10
+		} else {
+			speedData[p] = (int(chunkSize)*10/MB*1000/int(elapsed.Milliseconds()) + 5) / 10
+		}
+		print(".")
+	}
+	print("\n")
+
+	return true
+}
+
+func printSpeeds(measureRType string, speedData []int) {
+	fmt.Print(measureRType)
+	if Opts.KbSpeed {
+		fmt.Println(" (KB/s):")
+	} else {
+		fmt.Println(" (MB/s):")
+	}
+	fmt.Println(speedData)
 }
